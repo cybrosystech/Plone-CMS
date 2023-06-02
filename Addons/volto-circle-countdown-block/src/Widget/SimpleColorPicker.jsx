54e66cdc624870074123fe6ed7fffbb8e2bd3a10
Cybrosys Technologies <info@cybrosys.com>
2023-06-02 15:49:47 +0530

    Update SimpleColorPicker.jsx

--- Addons/volto-circle-countdown-block/src/Widget/SimpleColorPicker.jsx
+++ Addons/volto-circle-countdown-block/src/Widget/SimpleColorPicker.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import { FormFieldWrapper, Icon } from '@plone/volto/components';
 import { Button } from 'semantic-ui-react';
 import clearSVG from '@plone/volto/icons/clear.svg';
-import { SwatchesPicker } from 'react-color'
+import { SwatchesPicker } from 'react-color';
 
 
-import loadable from '@loadable/component';
-const GithubPicker = loadable(() => import('react-color/lib/Github'));
-
-export default (props) => {
+const SimpleColorPicker = (props) => {
   const { id, value, onChange } = props;
- 
+
   const [showPicker, setShowPicker] = React.useState(false);
 
   return (
@@ -42,23 +39,12 @@ export default (props) => {
         </Button.Group>
 
         {showPicker ? (
-          // <GithubPicker
-          //   width="220px"
-          //   className="color-picker"
-          //   colors={['#bbdbec', '#9dc6d4', '#5a93aa', '#005d7b', '#003d53', '#ebefc6', '#bdd494', '#6bb535', '#1e8339', '#025e37', '#464b0b', '#b5c234', '#777b1a', '#f4f1bc', '#e1e070', '#0070ae', '#fce6dc', '#f39a86', '#e73d5c', '#b92f47', '#8e1206', '#fff6a6', '#ffe525', '#f7a600', '#b94b19', '#8d4107', '#000000', '#6f6f6e', '#929291', '#bcbcbc', '#e3e3e3', '#ffffff']}
-          //   color={value || '#000'}
-          //   onChangeComplete={(value) => {
-          //     setShowPicker(false);
-          //     onChange(id, value.hex);
-          //   }}
-          // />
-          <SwatchesPicker 
-          onChangeComplete={(value) => {
-                  setShowPicker(false);
-                  onChange(id, value.hex);
-              
-              }}
-                />
+          <SwatchesPicker
+            onChangeComplete={(value) => {
+              setShowPicker(false);
+              onChange(id, value.hex);
+            }}
+          />
         ) : (
           ''
         )}
@@ -66,3 +52,5 @@ export default (props) => {
     </FormFieldWrapper>
   );
 };
+
+export default SimpleColorPicker;
